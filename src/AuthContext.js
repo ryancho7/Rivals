9edfef92264102fae7e1ca7d9266f52a2a6dad05
Ryan Cho <ryancho7@uw.edu>
2025-01-16 01:14:06 -0800

    app setup

---
+++ src/AuthContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from "react";
+import { auth } from "./Firebase";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setUser(user);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export { AuthContext, AuthProvider };
---
+++ src/pages/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/Landing.css'
+
+function Landing() {
+    return (
+        <h1 className="">Landing Page</h1>
+    )
+}
+
+export default Landing;
