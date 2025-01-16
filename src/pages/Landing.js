4ce7ef1d64623061f7b2453deed14498202523a0
Ryan Cho <ryancho7@uw.edu>
2025-01-16 11:53:05 -0800

    landing page ui

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 import '../styles/Landing.css'
 
 function Landing() {
     return (
-        <h1 className="">Landing Page</h1>
+        <div className="main-container">
+            {/* <h1 className="header">Marvel Rivals Just <span>Got Better</span></h1> */}
+            <h1 className="header">Welcome to <span>Rivals</span></h1>
+            <p className="subheader">Enter the ultimate battleground where heroes and villains report each other.</p>
+            <button className="login-button"><FcGoogle className="google-icon" size={24}/>Continue with Google</button>
+        </div>
     )
 }
 
