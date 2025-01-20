249b48de0b5f1b565b2deab4d92f11aee74385f5
ryancho7 <114904773+ryancho7@users.noreply.github.com>
2025-01-20 12:09:15 -0800

    dashboard ui

--- src/components/DashboardData.js
+++ src/components/DashboardData.js
@@ -1,8 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import '../styles/DashboardData.css';
 import { collection, doc, getDoc, getDocs, limit, orderBy, query, where } from "firebase/firestore";
 import { db } from "../Firebase";
 import { AuthContext } from "../AuthContext";
+import { IoTimerOutline } from "react-icons/io5";
+import { BsGraphUp, BsChatLeftText } from "react-icons/bs";
+import { FaRegEdit } from "react-icons/fa";
+
+import '../styles/DashboardData.css';
 
 function DashboardData() {
 
@@ -49,6 +53,10 @@ function DashboardData() {
         }
     },[user]);
 
+    if(!user) {
+        return <div>Loading ...</div>
+    }
+
     const getTimeSinceLastReport = () => {
         if (!userData || !userData.lastReport) return null;
         
@@ -63,27 +71,47 @@ function DashboardData() {
 
     return (
         <div className="data-container">
-            This is the data section
-            {timeSinceLastReport ? (
-                <div>It's been {timeSinceLastReport.hours} hours and {timeSinceLastReport.minutes} minutes since your last report</div>
-            ) : (
-                <div>You haven't made a report yet</div>
-            )}
-            <div className="recent-reports">
-                {userReports.length === 0 ? (
-                    <div className="no-reports">You haven't reported anyone recently</div>
-                ) : (
-                    userReports.map((report) => (
-                        <div key={report.id} className="report-item">
-                            <p>Reported Player: {report.reportedPlayer}</p>
-                            <p>Time: {report.timeOfReport.toDate().toLocaleString()}</p>
-                            <p>Likes: {report.likeCount}</p>
-                            <p>{report.comments}</p>
-                        </div>
-                    ))
-                )}
+                <div className="time-container">
+                    <div className="icon-area">
+                        <IoTimerOutline className="clock-icon"/>
+                    </div>
+                    <div className="text-area">
+                        {timeSinceLastReport ? (
+                            <div>It's been {timeSinceLastReport.hours} hours and {timeSinceLastReport.minutes} minutes since your last report</div>
+                        ) : (
+                            <div>You haven't made a report yet</div>
+                        )}
+                    </div>
+                </div>
+                <div className="report-count-container">
+                    <div className="icon-area">
+                        <BsGraphUp className="graph-icon"/>
+                    </div>
+                    <div className="text-area">
+                        {userData && (
+                            <div>You've reported {userData.reportCount} {userData.reportCount !== 1 ? "people" : "person"}</div>
+                        )}
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button className="report-feed-button"><BsChatLeftText /> Report Feed</button>
+                    <button className="edit-report-button"><FaRegEdit /> Edit Reports</button>
+                </div>
+                {/* <div className="recent-reports">
+                    {userReports.length === 0 ? (
+                        <div className="no-reports">You haven't reported anyone recently</div>
+                    ) : (
+                        userReports.map((report) => (
+                            <div key={report.id} className="report-item">
+                                <p>Reported Player: {report.reportedPlayer}</p>
+                                <p>Time: {report.timeOfReport.toDate().toLocaleString()}</p>
+                                <p>Likes: {report.likeCount}</p>
+                                <p>{report.comments}</p>
+                            </div>
+                        ))
+                    )}
+                </div> */}
             </div>
-        </div>
     )
 }
 
