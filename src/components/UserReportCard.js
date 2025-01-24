204c7747d420f859627baba577a2859e5177dfda
Ryan Cho <ryancho7@uw.edu>
2025-01-24 01:33:59 -0800

    responsive design + ui

--- src/components/UserReportCard.js
+++ src/components/UserReportCard.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { BiLike } from "react-icons/bi";
+import { FiUser } from "react-icons/fi";
+import { MdOutlineInsertComment } from "react-icons/md";
 import '../styles/UserReportCard.css';
 
 function UserReportCard({ report, backgroundImage }) {
@@ -8,9 +11,10 @@ function UserReportCard({ report, backgroundImage }) {
             style={{ backgroundImage: `url(${backgroundImage})` }}
         >
             <div className="main-content">
-                <p>Reported Player: {report.reportedPlayer}</p>
-                <p>Likes: {report.likeCount}</p>
-                <p>{report.comments}</p>
+                <p className="user-content"><FiUser />: {report.reportedPlayer}</p>
+                <hr />
+                <p className="comment-content"><MdOutlineInsertComment />: {report.comments}</p>
+                <p className="like-content">{report.likeCount}<BiLike /></p>
             </div>
             <div className="time-content">
                 <p>Reported At {report.timeOfReport.toDate().toLocaleString()}</p>
